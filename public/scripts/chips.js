bb0151f750b378edbf7572f9bb52aab081200c9c
anlit <46112266+CarlosGaray@users.noreply.github.com>
2022-04-07 19:19:06 -0500

    add course selected

--- public/scripts/chips.js
+++ public/scripts/chips.js
@@ -2,48 +2,41 @@
 setTimeout(() => {
     const selectCheckbox = document.querySelectorAll(".select-checkbox.sorting_3");
 
+    const tagContainer = document.querySelector('.tag-container');
+
     selectCheckbox.forEach(select => {
 
         if (select.getAttribute("rowspan")) {
-            console.log(select);
+            select.addEventListener("click", event => {
+
+                if(event.currentTarget.parentNode.classList[1] == undefined) {
+
+                    tagContainer.childNodes[1].textContent = "Cursos Seleccionados"
+
+                    tagContainer.insertAdjacentHTML("beforeend",
+                    `<div id="${event.currentTarget.parentNode.childNodes[0].textContent}_${event.currentTarget.parentNode.childNodes[2].textContent}">
+                        ${event.currentTarget.parentNode.childNodes[0].textContent}
+                        ${event.currentTarget.parentNode.childNodes[2].textContent}
+                        ${event.currentTarget.parentNode.childNodes[1].textContent}
+                    </div>`);
+                }
+
+                if(event.currentTarget.parentNode.classList[1] == "selected") {
+                    let idDelete = document.querySelector(`#${event.currentTarget.parentNode.childNodes[0].textContent}_${event.currentTarget.parentNode.childNodes[2].textContent}`);
+
+                    idDelete.remove();
+
+                    if(tagContainer.childNodes.length == 3) {
+                        tagContainer.childNodes[1].textContent = "";
+                    }
+
+                }
+
+            });
         } else {
             select.parentNode.style.setProperty("cursor", "not-allowed");
             select.parentNode.style.setProperty("pointer-events", "none");
-            console.log(select.parentNode);
         }
-
-        select.getAttribute("rowspan");
     });
 
 }, 2500);
-
-
-// const tagInput = document.querySelector("#input");
-
-// const form = document.forms[0];
-const tagContainer = document.querySelector('.tag-container');
-const tags = [];
-
-const createTag = (tagValue) => {
-    const value = tagValue.trim();
-
-    if (value === '' || tags.includes(value)) return;
-
-    const tag = document.createElement('span');
-    const tagContent = document.createTextNode(value);
-    tag.setAttribute('class', 'tag');
-    tag.appendChild(tagContent);
-
-    const close = document.createElement('span');
-    close.setAttribute('class', 'remove-tag');
-    close.innerHTML = '&#10006';
-    close.onclick = handleRemoveTag;
-
-    tag.appendChild(close);
-    tagContainer.appendChild(tag);
-    tags.push(tag);
-
-    tagInput.value = '';
-    tagInput.focus();
-}
-
