a69b06c6e217c2224df77bb6881479f1bdbbc1d0
anlit <46112266+CarlosGaray@users.noreply.github.com>
2022-04-08 02:10:19 -0500

    select cell paint

--- public/scripts/chips.js
+++ public/scripts/chips.js
@@ -2,36 +2,36 @@
 setTimeout(() => {
     const selectCheckbox = document.querySelectorAll(".select-checkbox.sorting_3");
 
+    
     const tagContainer = document.querySelector('.tag-container');
-
+    
     selectCheckbox.forEach(select => {
-
+        
+        
         if (select.getAttribute("rowspan")) {
-            select.addEventListener("click", event => {
+            // console.log(select.parentNode);
+            select.parentNode.addEventListener("click", event => {
+                console.log(event.currentTarget.classList);
 
-                if(event.currentTarget.parentNode.classList[1] == undefined) {
-
+                if(event.currentTarget.classList[1] != "selected") {
                     tagContainer.childNodes[1].textContent = "Cursos Seleccionados"
-
                     tagContainer.insertAdjacentHTML("beforeend",
-                    `<div class="content_select_course" id="${event.currentTarget.parentNode.childNodes[0].textContent}_${event.currentTarget.parentNode.childNodes[2].textContent}">
-                        ${event.currentTarget.parentNode.childNodes[0].textContent}
-                        ${event.currentTarget.parentNode.childNodes[2].textContent}
-                        <span class="text_course">${event.currentTarget.parentNode.childNodes[1].textContent}</span>
+                    `<div class="content_select_course" id="${event.currentTarget.childNodes[0].textContent}_${event.currentTarget.childNodes[2].textContent}">
+                        ${event.currentTarget.childNodes[0].textContent}
+                        ${event.currentTarget.childNodes[2].textContent}
+                        <span class="text_course">${event.currentTarget.childNodes[1].textContent}</span>
                     </div>`);
                 }
 
-                if(event.currentTarget.parentNode.classList[1] == "selected") {
-                    let idDelete = document.querySelector(`#${event.currentTarget.parentNode.childNodes[0].textContent}_${event.currentTarget.parentNode.childNodes[2].textContent}`);
+                if(event.currentTarget.classList[1] == "selected") {
+                    let idDelete = document.querySelector(`#${event.currentTarget.childNodes[0].textContent}_${event.currentTarget.childNodes[2].textContent}`);
 
                     idDelete.remove();
 
                     if(tagContainer.childNodes.length == 3) {
                         tagContainer.childNodes[1].textContent = "";
                     }
-
                 }
-
             });
         } else {
             select.parentNode.style.setProperty("cursor", "not-allowed");
